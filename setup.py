b5004f5796383478e68046ec394364a5cfe3d850
Nicolai Großer <nicolai.grosser@googlemail.com>
2023-11-19 16:45:53 +0100

    re-enabled Python 3.8 support

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     license="MIT",
     packages=find_packages(),
     install_requires=[],
-    python_requires=">=3.9",
+    python_requires=">=3.8",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
