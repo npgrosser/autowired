13ec68e8a793e16ccaae833fafcde939e98f8237
Nicolai Großer <nicolai.grosser@googlemail.com>
2023-11-27 00:15:30 +0100

    bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="autowired",
-    version="0.2.12",
+    version="0.2.13",
     description="A minimalistic dependency injection library for Python",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
